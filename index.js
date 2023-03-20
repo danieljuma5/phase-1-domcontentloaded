ec5c577879710532de4018843ad0749e8759e364
danieljuma5 <jumadaniel275@gmail.com>
2023-03-20 12:32:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+// Adding DOMContentLoaded event and changing p tag text
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM is Ready");
+  document.getElementById('text').innerHTML = "This is really cool!";
+});
